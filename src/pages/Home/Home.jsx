4cae7d0107d7b18cee0a3b68b8179c92a522adf1
walttoncoelho <walltton@outlook.com>
2023-02-02 22:34:04 -0300

    Cria o componente Nav Menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Navbar/>
+    menu
+    </>
+   
   )
 }
